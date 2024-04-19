c158d68b2f8f573c2ea46fff26ae9189d823da4f
Vishv Patel <139690991+ervip@users.noreply.github.com>
2024-04-19 17:04:56 +0530

    padding added to meal details screen

--- lib/screens/details.dart
+++ lib/screens/details.dart
@@ -13,45 +13,50 @@ class MealDetails extends StatelessWidget {
         title: Text(meal.title),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image.network(
-              meal.imageUrl,
-              width: double.infinity,
-              fit: BoxFit.cover,
-            ),
-            const SizedBox(height: 14),
-            Text(
-              'Ingredients',
-              style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                    color: Theme.of(context).colorScheme.primary,
-                    fontWeight: FontWeight.bold,
-                  ),
-            ),
-            const SizedBox(height: 14),
-            for (final ing in meal.ingredients)
-              Text(
-                "🛒  $ing",
-                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground),
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.network(
+                meal.imageUrl,
+                width: double.infinity,
+                fit: BoxFit.cover,
               ),
-            const SizedBox(height: 24),
-            Text(
-              'Steps To Cook',
-              style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                    color: Theme.of(context).colorScheme.primary,
-                    fontWeight: FontWeight.bold,
-                  ),
-            ),
-            const SizedBox(height: 14),
-            for (final (index, step) in meal.steps.indexed)
+              const SizedBox(height: 14),
               Text(
-                "${index + 1})  $step",
-                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground),
+                'Ingredients',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontWeight: FontWeight.bold,
+                    ),
               ),
-          ],
+              const SizedBox(height: 14),
+              for (final ing in meal.ingredients)
+                Text(
+                  "🛒  $ing",
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground),
+                  softWrap: true,
+                ),
+              const SizedBox(height: 24),
+              Text(
+                'Steps To Cook',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+              const SizedBox(height: 14),
+              for (final (index, step) in meal.steps.indexed)
+                Text(
+                  "${index + 1})  $step",
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground),
+                  softWrap: true,
+                ),
+            ],
+          ),
         ),
       ),
     );
